14eb778bb36d1093616e0e3853330a8a34036ab4
joyce.csb <joyce.csb@gmail.com>
2021-08-12 11:25:23 -0300

    add classe create pet

---
+++ src/test/java/petstore/Pet.java
@@ -0,0 +1,39 @@
+package petstore;
+
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static io.restassured.RestAssured.given;
+
+public class Pet {
+
+    String uri = "https://petstore.swagger.io/v2";
+
+    public String lerJson(String caminhoJson) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(caminhoJson)));
+    }
+
+    @Test  // Identifica o método ou função como um teste para o TestNG
+    public void incluirPet() throws IOException {
+        String jsonBody = lerJson("db/pet1.json");
+
+        // Sintaxe Gherkin
+        // Dado - Quando - Então
+        // Given - When - Then
+
+        given() // Dado
+                .contentType("application/json") // comum em API REST - antigas era "text/xml"
+                .log().all()
+                .body(jsonBody)
+                .when()  // Quando
+                .post(uri)
+                .then()  // Então
+                .log().all()
+                .statusCode(200)
+        ;
+
+    }
+}
