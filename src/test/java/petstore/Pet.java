8be07c374c0afb946fb2f55db07f4000ceb752ce
joyce.csb <joyce.csb@gmail.com>
2021-08-12 12:17:20 -0300

    update classe add pet

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -1,4 +1,7 @@
+// 1 - Pacote
 package petstore;
+
+// 2 - Bibliotecas
 
 import org.testng.annotations.Test;
 
@@ -8,14 +11,18 @@ import java.nio.file.Paths;
 
 import static io.restassured.RestAssured.given;
 
+
+// 3 - Classe
 public class Pet {
+    // 3.1 - Atributos
+    String uri = "https://petstore.swagger.io/v2/pet"; // endereço da entidade Pet
 
-    String uri = "https://petstore.swagger.io/v2";
-
+    // 3.2 - Métodos e Funções
     public String lerJson(String caminhoJson) throws IOException {
         return new String(Files.readAllBytes(Paths.get(caminhoJson)));
     }
 
+    // Incluir - Create - Post
     @Test  // Identifica o método ou função como um teste para o TestNG
     public void incluirPet() throws IOException {
         String jsonBody = lerJson("db/pet1.json");
@@ -35,5 +42,8 @@ public class Pet {
                 .statusCode(200)
         ;
 
+
     }
+
+
 }
